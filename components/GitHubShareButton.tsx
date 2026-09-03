0cb44a8c8e6590be204dec654512274707d80a49
Qubit-Shuo <AntiEntropy@pku.edu.cn>
2026-09-03 17:17:45 +0800

    fix: update github corner link

--- components/GitHubShareButton.tsx
+++ components/GitHubShareButton.tsx
@@ -5,7 +5,7 @@ import styles from './styles.module.css'
 export const GitHubShareButton: React.FC = () => {
   return (
     <a
-      href='https://github.com/Qubit-Fernand/Quantum-Computing'
+      href='https://github.com/Qubit-Fernand/pku-quantum'
       target='_blank'
       rel='noopener noreferrer'
       className={styles.githubCorner}
