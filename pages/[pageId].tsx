68a5fd70fca165cb87e516e8ae2bc91a4ad50f09
Qubit-Shuo <AntiEntropy@pku.edu.cn>
2026-09-03 17:23:08 +0800

    fix: restore notion page rendering

--- pages/[pageId].tsx
+++ pages/[pageId].tsx
@@ -30,7 +30,17 @@ export async function getStaticPaths() {
     }
   }
 
-  const siteMap = await getSiteMap()
+  let siteMap
+  try {
+    siteMap = await getSiteMap()
+  } catch (err) {
+    console.warn('failed to generate static page paths', err)
+
+    return {
+      paths: [],
+      fallback: true
+    }
+  }
 
   const staticPaths = {
     paths: Object.keys(siteMap.canonicalPageMap).map((pageId) => ({
